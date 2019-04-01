3500f205824079cc2d0250ed35cfc82a56c90f77
Juan <oqundo.juan@system-in-motion.com>
2019-04-01 17:51:40 +0800

    adding redux to detail

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,3 +23,14 @@ export function artistListAll(){
         payload: request
     }
 }
+
+
+export function artistDetail(id){
+    const request = axios.get(`${URL}/artists?id=${id}`)
+    .then(response => response.data)
+
+    return {
+        type: 'GET_ARTISTS_DETAIL',
+        payload: request
+    }
+}
--- src/reducers/artists_reducer.js
+++ src/reducers/artists_reducer.js
@@ -4,6 +4,9 @@ export default function(state={},action){
            return {...state,artistsList:action.payload} 
         case 'GET_ARTISTS_ALL':
            return {...state,artistsList:action.payload} 
+        
+        case 'GET_ARTISTS_DETAIL':
+            return {...state,artistData:action.payload} 
         default:
             return state;
     }
